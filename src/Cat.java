b77d228da4e86d15bc9098804296dd672cc5527f
d_nekh <dnekh2000@gmail.com>
2021-04-19 02:26:00 +0900

    There are Cat's class and Main class were created

---
+++ src/Cat.java
@@ -0,0 +1,48 @@
+public class Cat {
+
+
+    private String name;
+    private String color;
+    private int age;
+
+
+    public Cat() {
+        this("Mors", "Gray", 5);
+    }
+
+    public Cat(String name, String color, int age) {
+        this.name = name;
+        this.color = color;
+        this.age = age;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + this.name + ", Color: " + this.color + ", Age: " + this.age;
+    }
+}
